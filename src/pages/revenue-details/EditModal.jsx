1cf1b5a637e4b5b62078fa447db3efc5100bc096
Loc-Phan <63990538+Loc-Phan@users.noreply.github.com>
2024-03-18 14:23:05 +0700

    feedback ui

--- src/pages/revenue-details/EditModal.jsx
+++ src/pages/revenue-details/EditModal.jsx
@@ -5,7 +5,7 @@ import { api } from "../../provider/api";
 import { API_URL } from "../../config";
 
 const EditModal = ({ openEdit, handleCancel, item, setCheckSuceess }) => {
-  const [name, setName] = useState(item?.name || "");
+  // const [name, setName] = useState(item?.name || "");
   const [status, setStatus] = useState(item?.stauts || 0);
   const [rank, setRank] = useState(item?.rank || "A");
 
@@ -13,7 +13,7 @@ const EditModal = ({ openEdit, handleCancel, item, setCheckSuceess }) => {
     if (item?.app_id) {
       try {
         const payload = {
-          name,
+          // name,
           status,
           rank,
         };
@@ -41,37 +41,37 @@ const EditModal = ({ openEdit, handleCancel, item, setCheckSuceess }) => {
       onOk={handleEdit}
       onCancel={handleCancel}
     >
-      <div className="mt-4">
+      {/* <div className="mt-4">
         <p className="mb-2 text-base">Name</p>
         <Input
           name="name"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
-      </div>
-      <div className="mt-4">
-        <p className="mb-2 text-base">Status</p>
-        <Select
-          defaultValue={status}
-          style={{ width: 120 }}
-          onChange={(value) => setStatus(value)}
-          options={[
-            { value: 0, label: "Deactive" },
-            { value: 1, label: "Active" },
-          ]}
-        />
-      </div>
+      </div> */}
       <div className="mt-4">
         <p className="mb-2 text-base">Rank</p>
         <Select
           defaultValue={rank}
-          style={{ width: 120 }}
+          style={{ width: "100%" }}
           onChange={(value) => setRank(value)}
           options={[
             { value: "A", label: "A" },
             { value: "B", label: "B" },
             { value: "C", label: "C" },
             { value: "D", label: "D" },
+          ]}
+        />
+      </div>
+      <div className="mt-4">
+        <p className="mb-2 text-base">Status</p>
+        <Select
+          defaultValue={status}
+          style={{ width: "100%" }}
+          onChange={(value) => setStatus(value)}
+          options={[
+            { value: 0, label: "Deactive" },
+            { value: 1, label: "Active" },
           ]}
         />
       </div>
--- src/pages/revenue/DataTable.jsx
+++ src/pages/revenue/DataTable.jsx
@@ -10,6 +10,28 @@ const DataTable = ({ data }) => {
     navigate(`/revenue/${app_id}`);
   };
 
+  const groupedByRank = data?.list_app?.reduce((acc, app) => {
+    const rank = app.app_info.rank;
+    if (!acc[rank]) {
+      acc[rank] = [];
+    }
+    acc[rank].push(app);
+    return acc;
+  }, {});
+
+  const colorToRank = (item) => {
+    switch (item) {
+      case "A":
+        return "bg-[#13EC00]";
+      case "B":
+        return "bg-[#FFE500]";
+      case "C":
+        return "bg-[#FFA800]";
+      default:
+        return "bg-[#FF4D00]";
+    }
+  };
+
   return (
     <div className="mt-4 rounded-lg overflow-x-auto">
       {data?.list_app?.length > 0 ? (
@@ -173,169 +195,193 @@ const DataTable = ({ data }) => {
                   </td>
                 ))}
               </tr>
-              <tr className="rank">
-                <td colSpan="100">
-                  <div className="custom-sticky mt-1 text-xs text-black font-semibold bg-[#FAFF00] w-fit px-5 py-1 rounded-tr-2xl">
-                    Rank A
-                  </div>
-                </td>
-              </tr>
-              {data?.list_app?.map((item) => (
-                <React.Fragment key={item.app_id}>
-                  <tr className="custom-tr">
+              {Object.entries(groupedByRank || {}).map(([key, value]) => (
+                <React.Fragment>
+                  <tr className="rank">
                     <td colSpan="100">
-                      <div className="flex md:hidden px-2 gap-1 items-center">
-                        <div className={`w-2 h-2 rounded ${item?.app_info?.status === 1 ? "bg-[#00ED34]" : "bg-[#EA0000]"}`} />
-                        <p className="text-xs text-[#A3AED0] font-bold text-left ">
-                          {item?.app_info?.store_id}
-                        </p>
-                        <p className="text-sm text-main font-bold text-left">
-                          {item?.app_info?.app_name}
-                        </p>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr className="sticky-left">
-                    <td
-                      rowSpan={4}
-                      className="sticky text-center px-1 cursor-pointer hover:!bg-[#ebe9e9]"
-                      onClick={() => onHandleDetails(item?.app_info?.app_id)}
-                    >
-                      <div className="hidden md:flex gap-1 items-center">
-                        <div className={`w-2 h-2 rounded ${item?.app_info?.status === 1 ? "bg-[#00ED34]" : "bg-[#EA0000]"}`} />
-                        <Tooltip title={item?.app_info?.store_id}>
-                          <p className="text-xs text-[#A3AED0] font-bold text-left w-[10%] overflow-hidden truncate">
-                            {item?.app_info?.store_id}
-                          </p>
-                        </Tooltip>
-                        <Tooltip title={item?.app_info?.app_name}>
-                          <p className="text-sm text-main font-bold w-[90%] text-left overflow-hidden truncate">
-                            {item?.app_info?.app_name}
-                          </p>
-                        </Tooltip>
-                      </div>
-                      <div className="grid md:grid-cols-2">
-                        <div className="flex gap-1">
-                          <p>Doanh thu:</p>
-                          <p className="text-main font-bold">
-                            {item?.sum_revenue}
-                          </p>
-                        </div>
-                        <div className="flex gap-1">
-                          <p>Lợi nhuận:</p>
-                          <p className="text-main font-bold">
-                            {item?.sum_profit}
-                          </p>
-                        </div>
-                        <div className="flex gap-1">
-                          <p>Chi phí:</p>
-                          <p className="text-main font-bold">
-                            {item?.sum_cost}
-                          </p>
-                        </div>
-                        <div className="flex gap-1">
-                          <p>Tỷ lệ LN:</p>
-                          <p className="text-main font-bold">
-                            {item?.sum_profit_rate}%
-                          </p>
-                        </div>
-                      </div>
-                    </td>
-                    <td>S</td>
-                    <td>{item?.sum_cost === "0" ? "-" : item?.sum_cost}</td>
-                    {Object.values(item?.date_report || {}).map((value) => (
-                      <td key={Math.random()}>
-                        {value?.cost === "0" ? "-" : value?.cost}
-                      </td>
-                    ))}
-                  </tr>
-                  <tr className="sticky-left-1">
-                    <td>R</td>
-                    <td>
-                      {item?.sum_revenue === "0" ? "-" : item?.sum_revenuent}
-                    </td>
-                    {Object.values(item?.date_report || {}).map((value) => (
-                      <td key={Math.random()}>
-                        {value?.revenue === "0" ? "-" : value?.revenue}
-                      </td>
-                    ))}
-                  </tr>
-                  <tr className="sticky-left-1">
-                    <td>P</td>
-                    <td
-                      style={{
-                        background: item?.sum_profit?.includes("-")
-                          ? "#FF0000"
-                          : item?.sum_profit === "0"
-                          ? "#FFFA8C"
-                          : "#88F077",
-                        fontWeight: "bold",
-                        color: item?.sum_profit?.includes("-")
-                          ? "white"
-                          : "#435071",
-                      }}
-                    >
-                      {item?.sum_profit === "0" ? "-" : item?.sum_profit}
-                    </td>
-                    {Object.values(item?.date_report || {}).map((value) => (
-                      <td
-                        style={{
-                          background: value?.profit?.includes("-")
-                            ? "#FF0000"
-                            : value?.profit === "0"
-                            ? "#FFFA8C"
-                            : "#88F077",
-                          fontWeight: "bold",
-                          color: value?.profit?.includes("-")
-                            ? "white"
-                            : "#435071",
-                        }}
-                        key={Math.random()}
+                      <div
+                        className={`custom-sticky mt-1 text-xs text-black font-semibold ${colorToRank(
+                          key
+                        )} w-fit px-5 py-1 rounded-tr-2xl`}
                       >
-                        {value?.profit === "0" ? "-" : value?.profit}
-                      </td>
-                    ))}
-                  </tr>
-                  <tr className="sticky-left-1">
-                    <td>%</td>
-                    <td
-                      style={{
-                        background: item?.sum_profit_rate?.includes("-")
-                          ? "#FF0000"
-                          : item?.sum_profit_rate === "0"
-                          ? "#FFFA8C"
-                          : "#88F077",
-                        fontWeight: "bold",
-                        color: item?.sum_profit_rate?.includes("-")
-                          ? "white"
-                          : "#435071",
-                      }}
-                    >
-                      {item?.sum_profit_rate === "0"
-                        ? "-"
-                        : `${item?.sum_profit_rate}%`}
+                        {`Rank ${key}`}
+                      </div>
                     </td>
-                    {Object.values(item?.date_report || {}).map((value) => (
-                      <td
-                        style={{
-                          background: value?.profit_rate?.includes("-")
-                            ? "#FF0000"
-                            : value?.profit_rate === "0"
-                            ? "#FFFA8C"
-                            : "#88F077",
-                          fontWeight: "bold",
-                          color: value?.profit_rate?.includes("-")
-                            ? "white"
-                            : "#435071",
-                        }}
-                        key={Math.random()}
-                      >
-                        {value?.profit_rate === "0"
-                          ? "-"
-                          : `${value?.profit_rate}%`}
-                      </td>
-                    ))}
                   </tr>
+                  {value?.map((item) => (
+                    <React.Fragment key={item.app_id}>
+                      <tr className="custom-tr">
+                        <td colSpan="100">
+                          <div className="flex md:hidden px-2 gap-1 items-center">
+                            <div
+                              className={`w-2 h-2 rounded ${
+                                item?.app_info?.status === 1
+                                  ? "bg-[#00ED34]"
+                                  : "bg-[#EA0000]"
+                              }`}
+                            />
+                            <p className="text-xs text-[#A3AED0] font-bold text-left ">
+                              {item?.app_info?.store_id}
+                            </p>
+                            <p className="text-sm text-main font-bold text-left">
+                              {item?.app_info?.app_name}
+                            </p>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr className="sticky-left">
+                        <td
+                          rowSpan={4}
+                          className="sticky text-center px-1 cursor-pointer hover:!bg-[#ebe9e9]"
+                          onClick={() =>
+                            onHandleDetails(item?.app_info?.app_id)
+                          }
+                        >
+                          <div className="hidden md:flex gap-1 items-center">
+                            <div
+                              className={`w-2 h-2 rounded ${
+                                item?.app_info?.status === 1
+                                  ? "bg-[#00ED34]"
+                                  : "bg-[#EA0000]"
+                              }`}
+                            />
+                            <Tooltip title={item?.app_info?.store_id}>
+                              <p className="text-xs text-[#A3AED0] font-bold text-left w-[10%] overflow-hidden truncate">
+                                {item?.app_info?.store_id}
+                              </p>
+                            </Tooltip>
+                            <Tooltip title={item?.app_info?.app_name}>
+                              <p className="text-sm text-main font-bold w-[90%] text-left overflow-hidden truncate">
+                                {item?.app_info?.app_name}
+                              </p>
+                            </Tooltip>
+                          </div>
+                          <div className="grid md:grid-cols-2">
+                            <div className="flex gap-1">
+                              <p>Doanh thu:</p>
+                              <p className="text-main font-bold">
+                                {item?.sum_revenue}
+                              </p>
+                            </div>
+                            <div className="flex gap-1">
+                              <p>Lợi nhuận:</p>
+                              <p className="text-main font-bold">
+                                {item?.sum_profit}
+                              </p>
+                            </div>
+                            <div className="flex gap-1">
+                              <p>Chi phí:</p>
+                              <p className="text-main font-bold">
+                                {item?.sum_cost}
+                              </p>
+                            </div>
+                            <div className="flex gap-1">
+                              <p>Tỷ lệ LN:</p>
+                              <p className="text-main font-bold">
+                                {item?.sum_profit_rate}%
+                              </p>
+                            </div>
+                          </div>
+                        </td>
+                        <td>S</td>
+                        <td>{item?.sum_cost === "0" ? "-" : item?.sum_cost}</td>
+                        {Object.values(item?.date_report || {}).map((value) => (
+                          <td key={Math.random()}>
+                            {value?.cost === "0" ? "-" : value?.cost}
+                          </td>
+                        ))}
+                      </tr>
+                      <tr className="sticky-left-1">
+                        <td>R</td>
+                        <td>
+                          {item?.sum_revenue === "0"
+                            ? "-"
+                            : item?.sum_revenuent}
+                        </td>
+                        {Object.values(item?.date_report || {}).map((value) => (
+                          <td key={Math.random()}>
+                            {value?.revenue === "0" ? "-" : value?.revenue}
+                          </td>
+                        ))}
+                      </tr>
+                      <tr className="sticky-left-1">
+                        <td>P</td>
+                        <td
+                          style={{
+                            background: item?.sum_profit?.includes("-")
+                              ? "#FF0000"
+                              : item?.sum_profit === "0"
+                              ? "#FFFA8C"
+                              : "#88F077",
+                            fontWeight: "bold",
+                            color: item?.sum_profit?.includes("-")
+                              ? "white"
+                              : "#435071",
+                          }}
+                        >
+                          {item?.sum_profit === "0" ? "-" : item?.sum_profit}
+                        </td>
+                        {Object.values(item?.date_report || {}).map((value) => (
+                          <td
+                            style={{
+                              background: value?.profit?.includes("-")
+                                ? "#FF0000"
+                                : value?.profit === "0"
+                                ? "#FFFA8C"
+                                : "#88F077",
+                              fontWeight: "bold",
+                              color: value?.profit?.includes("-")
+                                ? "white"
+                                : "#435071",
+                            }}
+                            key={Math.random()}
+                          >
+                            {value?.profit === "0" ? "-" : value?.profit}
+                          </td>
+                        ))}
+                      </tr>
+                      <tr className="sticky-left-1">
+                        <td>%</td>
+                        <td
+                          style={{
+                            background: item?.sum_profit_rate?.includes("-")
+                              ? "#FF0000"
+                              : item?.sum_profit_rate === "0"
+                              ? "#FFFA8C"
+                              : "#88F077",
+                            fontWeight: "bold",
+                            color: item?.sum_profit_rate?.includes("-")
+                              ? "white"
+                              : "#435071",
+                          }}
+                        >
+                          {item?.sum_profit_rate === "0"
+                            ? "-"
+                            : `${item?.sum_profit_rate}%`}
+                        </td>
+                        {Object.values(item?.date_report || {}).map((value) => (
+                          <td
+                            style={{
+                              background: value?.profit_rate?.includes("-")
+                                ? "#FF0000"
+                                : value?.profit_rate === "0"
+                                ? "#FFFA8C"
+                                : "#88F077",
+                              fontWeight: "bold",
+                              color: value?.profit_rate?.includes("-")
+                                ? "white"
+                                : "#435071",
+                            }}
+                            key={Math.random()}
+                          >
+                            {value?.profit_rate === "0"
+                              ? "-"
+                              : `${value?.profit_rate}%`}
+                          </td>
+                        ))}
+                      </tr>
+                    </React.Fragment>
+                  ))}
                 </React.Fragment>
               ))}
             </tbody>
